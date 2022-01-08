205acc639f60b994ef025fbe05e9301fa8a53cbf
Christy Perozzi <christy.perozzi@gmail.com>
2022-01-08 15:27:11 -0500

    commit tree 8323133d6f1aab44c481adebc11318f494efe78b

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect( () => {
+    const timer = setTimeout( ()=> {
+      if (timeRemaining > 0){
+      setTimeRemaining( (timeRemaining)=> timeRemaining-1 )
+      }
+      else {handleAnswer(false)}
+
+    }, 1000)
+
+    // cleanup
+    return function cleanup() {
+      clearTimeout(timer);
+    };
+
+  })
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
